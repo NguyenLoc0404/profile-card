9d4a66df965ed60b66faffaa8acbf8a5fb0fa715
NguyenLoc0404 <nttloc0404git@gmail.com>
2025-05-21 10:53:36 +0700

    init data for profile card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const description =
+    "Full stack web developer and teach at Udemy. When not coding or preparing a couse, i like to play board games, to cook( and eat), or to just enjoy the Portufuese sun at the beach";
+  const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Express.js",
+  ];
+  const name = "Jonas Schmedtmans";
+  const imgSrc = "images/Recipe.jpg";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CardProfile
+        imgSrc={imgSrc}
+        name={name}
+        skills={skills}
+        description={description}
+      />
     </div>
   );
 }
+
+const CardProfile = (props) => {
+  return (
+    <div className="card">
+      <img src={props.imgSrc} alt="logo" />
+      <div className="container">
+        <h4>{props.name}</h4>
+        <p>{props.description}</p>
+        <Skills skills={props.skills} />
+      </div>
+    </div>
+  );
+};
+
+const Skills = (props) => {
+  return (
+    <div className="skills">
+      {props.skills.map((skill, index) => (
+        <span key={index}>{skill}</span>
+      ))}
+    </div>
+  );
+};
 
 export default App;
